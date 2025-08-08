b839d2df38ddf001cc83260cd44f582f72ce86e8
Sourav Kumar <souravkumar8432@gmail.com>
2025-08-08 18:25:16 +0530

    allwoed

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   preview: {
     host: true,
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: 'all',
   },
   plugins: [
     react(),
